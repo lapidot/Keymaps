4c5a7710d64c2a7d233bac4e97f936724230db7c
lapidot <lapidot@gmail.com>
2019-04-08 11:28:22 +0300

    Changed from Del/Ctrl on thumb key to Tab/Ctrl

--- keymap.c
+++ keymap.c
@@ -11,11 +11,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   SHFT_CAP, KC_Z,    KC_X,    KC_C,    LC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
   CTRLB,    TD(SUP), KC_LALT, KC_LCTL, TD(LOW), KC_SPC,      TD(RAI), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),*/
-  KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,    KC_T,           KC_Y,           KC_U,    KC_I,     KC_O,     KC_P,             KC_BSLASH,\
-  KC_ESC,  SHFT_A,   CTRL_S, ALT_D, GUI_F,    KC_G,           KC_H,           GUI_J,   ALT_K,    CTRL_L,   SHSQ,          TD(QUO),\
-  TD(CLS), Z_UNDO, X_REDO, KC_C,   KC_V,    KC_B,           KC_N,           KC_M,    CTRL_COMM,  SHFT_DOT,   SLASHES,   KC_RSHIFT,\
-  KC_TAB, KC_LGUI ,MIRR_TAB ,NUMPAD, KC_BSPC, CTLDEL,         SFT_ENT, 		KC_SPC,  ARROWS,   FN_ESC, KC_EQUAL, CPYPST,\
- _______, _______, _______, _______, _______,    _______,  _______, _______,      _______, _______, _______ \
+  KC_TAB,  KC_Q,   KC_W,    KC_E,   KC_R,    KC_T,	KC_Y,	KC_U,    KC_I,     KC_O,     KC_P,	KC_BSLASH,\
+  MIRR_ESC,SHFT_A, CTRL_S,  ALT_D,  GUI_F,   KC_G,	KC_H,	GUI_J,   ALT_K,    CTRL_L,   SHSQ,	TD(QUO),\
+  TD(CLS), Z_UNDO, X_REDO,  KC_C,   KC_V,    KC_B,	KC_N,	KC_M,    CTRL_COMM,SHFT_DOT, SLASHES,   KC_RSHIFT,\
+  KC_TAB,  KC_LGUI,MIRR_DEL,NUMPAD, KC_BSPC, CTRL_TAB,	SFT_ENT,KC_SPC,  ARROWS,   FN_ESC,   KC_EQUAL,	CPYPST,\
+ _______, _______, _______, _______, _______,_______,	_______,_______, _______,  _______,  _______ \
 ), 
 
 /*BATREUS: 
@@ -29,10 +29,10 @@ KC_TAB,  KC_ESC,KC_LGUI,NUMPAD, KC_BSPC, CTLDEL,         SFT_ENT, KC_SPC,  ARROW
 */
 
 [_MIRROR] = LAYOUT_preonic_1x2uC(  /*MIRROR*/
-  KC_TAB,       KC_P,           KC_O,    KC_I,     KC_U,    KC_Y,           KC_MS_ACCEL0, KC_MS_BTN1,	KC_MS_UP,  KC_MS_BTN2, KC_Q,KC_BSLASH, \
-  TO(_WINBASE), TD(SCL),        KC_L,    KC_K,     KC_J,    KC_H,           KC_MS_ACCEL1, KC_MS_LEFT,	KC_MS_DOWN,KC_MS_RIGHT,KC_A,TD(QUO), \
-  TD(CLS),      CTL_T(KC_SLASH),KC_DOT,  KC_COMM,  KC_M,    KC_N,           KC_MS_ACCEL2, KC_V,		KC_MS_DOWN,KC_X,       KC_Z,KC_RSHIFT, \
-  KC_EQUAL,     KC_MINUS,	_______, _______,  KC_SPC,  KC_ENT,	    LGUI(KC_DEL), KC_BSPC,	KC_TAB, TO(_WINBASE),_______, _______, \
+  KC_BSLASH,  KC_P,           KC_O,    KC_I,     KC_U,    KC_Y,           KC_MS_ACCEL0, KC_MS_BTN1,	KC_MS_UP,  KC_MS_BTN2, KC_Q,KC_BSLASH, \
+  _______, TD(SCL),        KC_L,    KC_K,     KC_J,    KC_H,           KC_MS_ACCEL1, KC_MS_LEFT,	KC_MS_DOWN,KC_MS_RIGHT,KC_A,TD(QUO), \
+  TD(CLS), CTL_T(KC_SLASH),KC_DOT,  KC_COMM,  KC_M,    KC_N,           KC_MS_ACCEL2, KC_V,		KC_MS_DOWN,KC_X,       KC_Z,KC_RSHIFT, \
+  KC_EQUAL,KC_MINUS,	_______, _______,  KC_SPC,  KC_ENT,	    LGUI(KC_DEL), KC_BSPC,	KC_TAB, TO(_WINBASE),_______, _______, \
   _______, _______,         _______, _______, _______,          _______, _______, _______,       _______, _______, _______ \
   ),
 /*BATREUS:
@@ -45,11 +45,11 @@ CPYPST, KC_EQUAL, KC_MINUS,       _______,   KC_SPC, RSFT_T(KC_ENT),LGUI(KC_DEL)
 
 
 [_ARROWS] = LAYOUT_preonic_1x2uC(  /* RAISE - Arrows */
-  ALT_TAB, KC_LALT,   XXXXXXX,	 KC_PIPE,   KC_TILD,	XXXXXXX, XXXXXXX,  KC_HOME, KC_UP,   KC_PGUP,  XXXXXXX,   _______,\
-  _______, TD(CLS), CURLY,	 SQUAR,	    PARAN,	XXXXXXX, KC_PSCR,  KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX,   _______,\
-  _______, LCTL(KC_Z),LCTL(KC_X),LCTL(KC_C),LCTL(KC_V), XXXXXXX, XXXXXXX,  KC_END,  KC_DOWN, KC_PGDN,  XXXXXXX,   _______,\
-  TO(_WINBASE),KC_APP,_______,   _______,   _______,	_______, _______,  _______, _______, _______,  _______, _______, \
-  _______, _______,   _______,   _______,   _______,	_______, _______,  _______, _______, _______, _______\
+  ALT_TAB, 	KC_LALT,   	XXXXXXX,	KC_PIPE,   	KC_TILD,	XXXXXXX, XXXXXXX,  KC_HOME, KC_UP,   KC_PGUP,  XXXXXXX,   _______,\
+  _______, 	_______,   	_______, 	_______, 	_______,	XXXXXXX, KC_PSCR,  KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX,   _______,\
+  _______, 	LCTL(KC_Z),	LCTL(KC_X),	LCTL(KC_C),	LCTL(KC_V),	XXXXXXX, XXXXXXX,  KC_END,  KC_DOWN, KC_PGDN,  XXXXXXX,   _______,\
+  TO(_WINBASE),	KC_APP,		_______,   	_______,   	CTLDEL,	_______, _______,  _______, _______, _______,  _______, _______, \
+  _______, 	_______,   	_______,   	_______,   	_______,	_______, _______,  _______, _______, _______, _______\
 /*
 BATreus
   _______, _______, FUNCTN, _______, _______, _______, _______,  _______, _______, FUNCTN,  _______, _______, \
@@ -59,24 +59,8 @@ Atreus
 
 */
 
-),
-
-
-[_FUNC] = LAYOUT_preonic_1x2uC(  /* ADJUST - Macros, Layer Switching, Function Keys */
-/*  UNIWIN,  XXXXXXX, XXXXXXX, RANDIG,  RANDIG,  KC_INS,  XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, AU_TOG, MU_TOG,  \
-  UNILIN,  SUPA2,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F1,    KC_F2,   KC_F3,   KC_F4,  XXXXXXX, \
-  XXXXXXX, DEGREE,  IBANG,   LAROW,   RAROW,   SWCOLE,  COLEMAK, KC_F5,    KC_F6,   KC_F7,   KC_F8,  BL_INC,  \
-  _______, CADKEY,  MICRO,   WOMEGA,  OMEGA,   XXXXXXX, XXXXXXX, KC_F9,    KC_F10,  KC_F11,  KC_F12, BL_DEC,  \
-  _______, _______, _______, _______, _______,      RESET,       _______,  XXXXXXX, MUV_DE,  MUV_IN, BL_TOGG  \*/
-
-  _______,	KC_VOLU,   RESET,  	XXXXXXX, XXXXXXX, KC_APP,	XXXXXXX,KC_F7,  KC_F8,   KC_F9, KC_F10,  KC_F11,  \
-  TO(_WINBASE), KC_LSHIFT, KC_LCTRL, 	KC_LALT, KC_LGUI, LCTL(KC_LALT),XXXXXXX,KC_F4,   KC_F5,  KC_F6, KC_F11,  XXXXXXX, \
-  XXXXXXX, 	KC_CAPS,   KC_INSERT,	XXXXXXX, KC_F,    KC_PSCR,	XXXXXXX,KC_F1,  KC_F2,   KC_F3, KC_F12,  KC_F12,  \
-  RESET, 	_______,   _______,	_______, _______, _______,	_______,_______,_______, _______,_______, _______,  \
-  _______, 	_______,   _______,	_______, _______, RESET,	_______,XXXXXXX,MUV_DE,  MUV_IN, BL_TOGG  \
 
 ),
-
 
 [_NUMPAD] = LAYOUT_preonic_1x2uC(  /* LOWER - Numpad */
 /*  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_QUES, KC_DQT,  KC_DEL,  \
@@ -88,8 +72,9 @@ Atreus
   ALT_TAB, KC_VOLU, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,		KC_ASTR,KC_7,   KC_8,	KC_9,   KC_0,    _______,\
   LANGSWP, KC_VOLD, CURLY,   SQUAR,   PARAN,    XXXXXXX,	KC_SLSH,KC_4,   KC_5,	KC_6,   KC_PLUS, KC_ASTR,\
   _______, KC_MUTE, KC_CUT,  KC_COPY, KC_PASTE, KC_PSCR,	KC_0,	KC_1,   KC_2,	KC_3,	KC_MINS, KC_PLUS,\
-  _______, KC_APP,  _______, _______, _______,  _______,	_______,_______,KC_KP_DOT,TO(_WINBASE), _______, _______, \
+  _______, KC_APP,  TO(_WINBASE), _______, CTLDEL,_______,	_______,_______,_______,KC_KP_DOT, _______, _______, \
   _______, _______, _______, _______, _______,  _______, 	_______,_______,_______,_______, _______\
+
 /*
 BATreus
   _______, _______, FUNCTN, _______, _______, _______, _______,  _______, _______, FUNCTN,  _______, _______, \
@@ -98,6 +83,22 @@ Atreus
   _______, _______, _______, FUNCTN, _______, _______, _______,  _______ ,_______,_______,  _______, _______, \
 
 */
+),
+
+[_FUNC] = LAYOUT_preonic_1x2uC(  /* ADJUST - Macros, Layer Switching, Function Keys */
+/*  UNIWIN,  XXXXXXX, XXXXXXX, RANDIG,  RANDIG,  KC_INS,  XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, AU_TOG, MU_TOG,  \
+  UNILIN,  SUPA2,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F1,    KC_F2,   KC_F3,   KC_F4,  XXXXXXX, \
+  XXXXXXX, DEGREE,  IBANG,   LAROW,   RAROW,   SWCOLE,  COLEMAK, KC_F5,    KC_F6,   KC_F7,   KC_F8,  BL_INC,  \
+  _______, CADKEY,  MICRO,   WOMEGA,  OMEGA,   XXXXXXX, XXXXXXX, KC_F9,    KC_F10,  KC_F11,  KC_F12, BL_DEC,  \
+  _______, _______, _______, _______, _______,      RESET,       _______,  XXXXXXX, MUV_DE,  MUV_IN, BL_TOGG  \*/
+
+  _______,	KC_VOLU,   RESET,  	XXXXXXX, XXXXXXX, KC_APP,	XXXXXXX,KC_F7,  KC_F8,   KC_F9, KC_F10,  KC_F11,  \
+  TO(_WINBASE), KC_LSHIFT, KC_LCTRL, 	KC_LALT, KC_LGUI, LCTL(KC_LALT),XXXXXXX,KC_F4,  KC_F5,   KC_F6, KC_F11,  XXXXXXX, \
+  XXXXXXX, 	KC_CAPS,   KC_INSERT,	XXXXXXX, KC_F,    KC_PSCR,	XXXXXXX,KC_F1,  KC_F2,   KC_F3, KC_F12,  KC_F12,  \
+  RESET, 	_______,   _______,	_______, _______, _______,	_______,_______,_______, _______,_______, _______,  \
+  _______, 	_______,   _______,	_______, _______, RESET,	_______,XXXXXXX,MUV_DE,  MUV_IN, BL_TOGG  \
 
 )
+
+
 };
